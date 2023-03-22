846ee3153e1695799f7702db09d5d877991a7b6e
Varun Kumar T P <63554822+varunkumartp@users.noreply.github.com>
2023-03-22 12:28:50 +0530

    Update database.ts
    Included Comments

--- src/database/database.ts
+++ src/database/database.ts
@@ -1,3 +1,9 @@
+// In the react-native-sqlite-storage library folder, go to react-native-config and comment the following part
+// ios: {
+// 	project: './platforms/ios/SQLite.xcodeproj'
+// },
+// If this is not commented, database can't be linked to project
+
 import SQLite from 'react-native-sqlite-storage';
 
 export const db = SQLite.openDatabase(
